92c316ec10ec8ef099e668696ae4e4223fa69324
Fernando Zurita <lzurita@infracommerce.com>
2024-09-25 10:13:05 -0600

    add: cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routes.routing_problems import routing_problems
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routing_problems)
